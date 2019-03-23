134bc9d1529d0348d8df9064f6c37da2ac3c4cef
GugaGongadze <ggong13@freeuni.edu.ge>
2019-03-23 22:49:29 +0400

    Feat: book implementation of weighted quick union

---
+++ reverie 0 - Union−Find/book/2-weighted-quick-union.js
@@ -0,0 +1,37 @@
+class WeightedQuickUnionUF {
+  constructor(N) {
+    this.id = new Array(N)
+    this.sz = new Array(N).fill(1)
+
+    for (let i = 0; i < N; i++) {
+      this.id[i] = i
+    }
+  }
+
+  root(i) {
+    while (i != this.id[i]) {
+      i = this.id[i]
+    }
+
+    return i
+  }
+
+  connected(p, q) {
+    return this.root(p) === this.root(q)
+  }
+
+  union(p, q) {
+    const i = this.root(p)
+    const j = this.root(q)
+
+    if (i === j) return
+    
+    if (this.sz[i] < this.sz[j]) {
+      this.id[i] = j
+      this.sz[j] += this.sz[i]
+    } else { 
+      this.id[j] = i
+      this.sz[i] += this.sz[j]
+    }
+  }
+}
